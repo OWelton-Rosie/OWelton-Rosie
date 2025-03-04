a4927e2f07944d50befced44050a8e0e3dc8b8ef
Oscar Welton-Rosie <185318677+OWelton-Rosie@users.noreply.github.com>
2025-03-04 15:12:19 +1300

    Delete website/date_time.js

--- website/date_time.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    const now = new Date();
-    const time = now.toLocaleTimeString("en-NZ", { timeZone: "Pacific/Auckland", hour: "2-digit", minute: "2-digit" });
-    const date = now.toLocaleDateString("en-NZ", { timeZone: "Pacific/Auckland", year: "numeric", month: "long", day: "numeric" });
-    document.getElementById("timeDisplay").innerHTML = `<b>${time}</b>, ${date}`;
-};
