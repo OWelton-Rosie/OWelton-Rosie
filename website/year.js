129d8322f7059cb7e4a4ea857c764c6b0456deba
Oscar Welton-Rosie <185318677+OWelton-Rosie@users.noreply.github.com>
2025-04-23 17:19:50 +1200

    Create year.js

---
+++ website/year.js
@@ -0,0 +1,5 @@
+        // Get the current year
+        const currentYear = new Date().getFullYear();
+
+        // Insert the current year into the element with the id "currentYear"
+        document.getElementById("currentYear").textContent = currentYear;
