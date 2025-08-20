0273de153206df795904226eb68507cc642917e3
Oscar Welton-Rosie <185318677+OWelton-Rosie@users.noreply.github.com>
2025-08-21 07:28:19 +1200

    Update photos.js

--- website/scripts/photos.js
+++ website/scripts/photos.js
@@ -1,17 +1,17 @@
 const photos = [
     { src: "assets/photos/IMG_6435.JPG", caption: "Sailboats on the water, Wellington Harbour." },
     { src: "assets/photos/IMG_6441.JPG", caption: "The rocks of Wellington Harbour." },
-    { src: "assets/photos/IMG_6697.JPG", caption: "The pools and rocks of Island Bay." },
+    { src: "assets/photos/IMG_6697.JPG", caption: "The rockpools of Island Bay." },
     { src: "assets/photos/IMG_6716.JPG", caption: "The Sun illuminating the Liardet Street park football fields." },
     { src: "assets/photos/IMG_6720.JPG", caption: "Vew of Wellington city from the top of Hutchison Road." },
     { src: "assets/photos/IMG_6792.JPG", caption: "Wellington city, seen from Liardet Street park." },
-    { src: "assets/photos/IMG_6804.JPG", caption: "Looking out at Wellington city, with some crazy cloud formations" },
+    { src: "assets/photos/IMG_6804.JPG", caption: "Looking out at Wellington city, with cloud formations creating a beautiful skyline" },
     { src: "assets/photos/IMG_7028.JPG", caption: "View of Red Rocks." },
-    { src: "assets/photos/IMG_7740.JPG", caption: "Oriental Bay." },
+    { src: "assets/photos/IMG_7740.JPG", caption: "View of Oriental Bay." },
     { src: "assets/photos/IMG_7799.JPG", caption: "Looking out at Wellington city from the streets of Mt Victoria" },
     { src: "assets/photos/IMG_6999.JPG", caption: "Looking out at Wellington city from the Mt Victoria lookout" },
     { src: "assets/photos/IMG_6718.JPG", caption: "Morning Sun seen from the foot of Dransfield Street." },
-    {src: "assets/photos/IMG_7024.JPG", caption: "A horse at the Manawa Karioi restoration project." },
+    {src: "assets/photos/IMG_7024.JPG", caption: "A horse at the Manawa Karioi ecological restoration project." },
 
 
   ];
