a9beeadb3b9e912f9ebc3c26f715a41602cce2ed
Oscar Welton-Rosie <185318677+OWelton-Rosie@users.noreply.github.com>
2025-08-21 11:17:10 +1200

    make some captions better

--- website/scripts/photos.js
+++ website/scripts/photos.js
@@ -6,7 +6,7 @@ const photos = [
     { src: "assets/photos/IMG_6720.JPG", caption: "Vew of Wellington city from the top of Hutchison Road." },
     { src: "assets/photos/IMG_6792.JPG", caption: "Wellington city, seen from Liardet Street park." },
     { src: "assets/photos/IMG_6804.JPG", caption: "Looking out at Wellington city, with cloud formations creating a beautiful skyline" },
-    { src: "assets/photos/IMG_7028.JPG", caption: "View of Red Rocks." },
+    { src: "assets/photos/IMG_7028.JPG", caption: "View of the Red Rocks hills." },
     { src: "assets/photos/IMG_7740.JPG", caption: "View of Oriental Bay." },
     { src: "assets/photos/IMG_7799.JPG", caption: "Looking out at Wellington city from the streets of Mt Victoria" },
     { src: "assets/photos/IMG_6999.JPG", caption: "Looking out at Wellington city from the Mt Victoria lookout" },
@@ -14,9 +14,9 @@ const photos = [
     { src: "assets/photos/IMG_7024.JPG", caption: "A horse at the Manawa Karioi ecological restoration project." },
     { src: "assets/photos/IMG_6951.JPG", caption: "Wellington City, seen from the top of Central Park." },
     { src: "assets/photos/IMG_6805.JPG", caption: "Wellington City and Harbour." },
-    { src: "assets/photos/IMG_6806.JPG", caption: "Cloud formations forming a beautiful skyline." },
-    { src: "assets/photos/IMG_6989.JPG", caption: "Wellington Airport, seen from the top of Island Bay." },
-    { src: "assets/photos/IMG_7027.JPG", caption: "The hills of Red Rocks." },
+    { src: "assets/photos/IMG_6806.JPG", caption: "Cloud formations creating a beautiful skyline." },
+    { src: "assets/photos/IMG_6989.JPG", caption: "Wellington Airport, seen from the hills of Island Bay." },
+    { src: "assets/photos/IMG_7027.JPG", caption: "The hills and coastline of Red Rocks." },
     { src: "assets/photos/IMG_7587.JPG", caption: "The rugged coastline of Breaker Bay." },
 
 
