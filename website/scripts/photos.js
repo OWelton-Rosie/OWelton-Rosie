de35b63f1414aafa80a91ff95c22826a30658e21
Oscar Welton-Rosie <185318677+OWelton-Rosie@users.noreply.github.com>
2025-08-21 09:17:08 +1200

    more photos + fix indents

--- website/scripts/photos.js
+++ website/scripts/photos.js
@@ -11,7 +11,13 @@ const photos = [
     { src: "assets/photos/IMG_7799.JPG", caption: "Looking out at Wellington city from the streets of Mt Victoria" },
     { src: "assets/photos/IMG_6999.JPG", caption: "Looking out at Wellington city from the Mt Victoria lookout" },
     { src: "assets/photos/IMG_6718.JPG", caption: "Morning Sun seen from the foot of Dransfield Street." },
-    {src: "assets/photos/IMG_7024.JPG", caption: "A horse at the Manawa Karioi ecological restoration project." },
+    { src: "assets/photos/IMG_7024.JPG", caption: "A horse at the Manawa Karioi ecological restoration project." },
+    { src: "assets/photos/IMG_6951.JPG", caption: "Wellington City, seen from the top of Central Park." },
+    { src: "assets/photos/IMG_6805.JPG", caption: "Wellington City and Harbour." },
+    { src: "assets/photos/IMG_6806.JPG", caption: "Cloud formations forming a beautiful skyline." },
+    { src: "assets/photos/IMG_6989.JPG", caption: "Wellington Airport, seen from the top of Island Bay." },
+    { src: "assets/photos/IMG_7027.JPG", caption: "The hills of Red Rocks." },
+    { src: "assets/photos/IMG_7587.JPG", caption: "The rugged coastline of Breaker Bay." },
 
 
   ];
