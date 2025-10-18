15e3890a720e197ffbc32bedc41c258bd49bd9fb
Oscar Welton-Rosie <oweltonrosie@gmail.com>
2025-10-18 16:41:47 +1300

    more photo protection

--- website/scripts/protect_photos.js
+++ website/scripts/protect_photos.js
@@ -1,6 +1,7 @@
 const photo = document.getElementById("photo");
 const message = document.getElementById("rightClickMessage");
 
+// Disable right-click
 photo.addEventListener("contextmenu", e => {
   e.preventDefault();
 
@@ -12,3 +13,10 @@ photo.addEventListener("contextmenu", e => {
     message.style.opacity = "0";
   }, 2500);
 });
+
+// Disable dragging
+photo.ondragstart = () => false;
+
+// Disable text selection
+photo.onselectstart = () => false;
+photo.style.userSelect = "none"; // CSS fallback
