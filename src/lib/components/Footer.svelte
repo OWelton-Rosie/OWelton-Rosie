1df8f33cd0542e0f38c32f531c81c973b121e2b5
Oscar Welton-Rosie <oweltonrosie@gmail.com>
2025-11-20 12:06:33 +1300

    update footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,6 @@
+<footer>
 <p>Copyright © Oscar Welton-Rosie, 2025</p>
+</footer>
 
 <style>
     p {
@@ -7,4 +9,14 @@
         color: #666;
         margin: 1em 0;
     }
+
+    /* Footer */
+footer {
+    text-align: center;
+    padding-top: 2rem;
+    margin-top: 4rem;
+    font-size: 0.9rem;
+    color: #6b7280;
+    border-top: 1px solid #e5e7eb;
+}
 </style>
