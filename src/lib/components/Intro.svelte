aa071d154a4a6a16559d239782cb9736844202e4
Oscar Welton-Rosie <oweltonrosie@gmail.com>
2026-02-03 17:04:35 +1300

    1-pager

--- src/lib/components/Intro.svelte
+++ src/lib/components/Intro.svelte
@@ -1,28 +1,3 @@
-<div class="hero-content">
-    <img src="/assets/portrait.jpg" alt="Oscar Welton-Rosie Portrait" class="hero-portrait" />
-<h1>Oscar Welton-Rosie</h1>
-<p>(That's me and you arrived at my website.)</p>
-</div>
+<p>Hey,</p>
 
-<p>I'm a New Zealand high school student, <a href="/projects">web developer</a>, <a href="/speedcubing">speedcuber</a>, <a href="https://www.strava.com/athletes/120805648" target="_blank" rel="noopener noreferrer">athlete</a> and Formula 1 fan.</p>
-
-
-<p>On this site, you can <a href="/about">find out more about me</a>, learn about my hobby of <a href="/speedcubing">speedcubing</a>, read some of the <a href="/blog">things I've written</a>, and look at some of the <a href="/photos">photos I've taken</a>.</p>
-
-<style>
-img {
-    display: block;     /* Make it block so margin works */
-    margin: 0 auto;     /* Centers horizontally */
-    width: 50%;
-    height: 50%;
-}
-
-.hero-content {
-    text-align: center;
-    margin-bottom: 10%;
-}
-
-.hero-content h1 {
-    font-size: 3rem;
-}
-</style>
+<p>I'm Oscar. I'm a New Zealand high school student, hobby web developer, and <a href="https://www.strava.com/athletes/120805648" target="_blank" rel="noopener noreferrer">athlete</a>. If you want to learn more about me, you could check out my <a href="https://github.com/OWelton-Rosie"target="_blank", rel="noopener noreferrer">GitHub</a>, but the best way is to <a href="mailto:oweltonrosie@gmail.com" target="_blank", rel="noopener noreferrer">email me</a>.</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,32 @@
 <script>
-    import Header from '$lib/components/Header.svelte';
     import Intro from '$lib/components/Intro.svelte';
     import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<Header />
+<div class="hero-content">
+    <img src="/assets/portrait.jpg" alt="Oscar Welton-Rosie Portrait" class="hero-portrait" />
+<h1>Oscar Welton-Rosie</h1>
+</div>
+
 <Intro />
+
+
+<style>
+img {
+    display: block;     /* Make it block so margin works */
+    margin: 0 auto;     /* Centers horizontally */
+    width: 40%;
+    height: 40%;
+}
+
+.hero-content {
+    text-align: center;
+}
+
+.hero-content h1 {
+    font-size: 3rem;
+}
+</style>
 <Footer />
 
 
