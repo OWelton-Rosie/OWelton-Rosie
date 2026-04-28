08971ec5ea1de3bd3ddd02eedc61ffc0e238e702
Oscar Welton-Rosie <oweltonrosie@gmail.com>
2026-04-28 13:42:53 +1200

    fix missing fullstop

--- src/lib/components/Intro.svelte
+++ src/lib/components/Intro.svelte
@@ -7,4 +7,4 @@
 
 <p>I'm currently working with <a href="https://github.com/OWelton-Rosie?tab=repositories&q=&type=&language=python&sort=" target="_blank" rel="noopener noreferrer">Python</a> and <a href="https://github.com/OWelton-Rosie?tab=repositories&q=&type=&language=svelte&sort=" target="_blank" rel="noopener noreferrer">Svelte</a>, but I've built <a href="https://github.com/OWelton-Rosie?tab=repositories&q=&type=&language=javascript&sort=" target="_blank" rel="noopener noreferrer">some stuff in JavaScript</a>.</p>
 
-<p>When not coding, I enjoy distance running and <a href="https://www.worldcubeassociation.org/persons/2023WELT02" target="_blank" rel="noopener noreferrer">solving Rubik's Cubes</a></p>
+<p>When not coding, I enjoy distance running and <a href="https://www.worldcubeassociation.org/persons/2023WELT02" target="_blank" rel="noopener noreferrer">solving Rubik's Cubes</a>.</p>
