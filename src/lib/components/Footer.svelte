264a288a5e44710bda6618dcaa8a6e74c82a5760
Oscar Welton-Rosie <oweltonrosie@gmail.com>
2026-06-19 09:37:37 +1200

    will this work

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,22 @@
+<footer>
+<p>Copyright © Oscar Welton-Rosie, 2026</p>
+</footer>
+
+<style>
+    p {
+        text-align: center;
+        font-size: 0.9em;
+        color: #666;
+        margin: 1em 0;
+    }
+
+    /* Footer */
+footer {
+    text-align: center;
+    padding-top: 2rem;
+    margin-top: 4rem;
+    font-size: 0.9rem;
+    color: #6b7280;
+    border-top: 1px solid #e5e7eb;
+}
+</style>
---
+++ src/lib/components/Intro.svelte
@@ -0,0 +1,7 @@
+<p>Hi,</p>
+
+<p>I'm Oscar. I'm a New Zealand high school student and hobby web developer.</p>
+
+<p> I enjoy using my skills to build solutions to problems, and I'm currently working with <a href="https://github.com/OWelton-Rosie?tab=repositories&q=&type=&language=python&sort=" target="_blank" rel="noopener noreferrer">Python</a>, <a href="https://github.com/OWelton-Rosie?tab=repositories&q=&type=&language=svelte&sort=" target="_blank" rel="noopener noreferrer">Svelte</a>, and <a href="https://github.com/OWelton-Rosie?tab=repositories&q=&type=&language=javascript&sort=" target="_blank" rel="noopener noreferrer">JavaScript</a>.</p>
+
+<p>When not coding, I enjoy <a href="https://www.strava.com/athletes/120805648" target="_blank" rel="noopener noreferrer">running</a> and <a href="https://www.worldcubeassociation.org/persons/2023WELT02" target="_blank" rel="noopener noreferrer">solving Rubik's Cubes</a>.</p>
