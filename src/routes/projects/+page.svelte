fa5e3817fb91f837727763ca0c6795939f6a4a74
Oscar Welton-Rosie <oweltonrosie@gmail.com>
2026-08-10 09:22:07 +1200

    line break

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -42,6 +42,7 @@
   <a href="https://github.com/OWelton-Rosie/OWelton-Rosie" target="_blank" rel="noopener noreferrer">Source code</a>
 </p>
 
+<br>
 <p>Other projects can be viewed on my <a href="https://github.com/OWelton-Rosie?tab=repositories" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
 
 <Footer />
