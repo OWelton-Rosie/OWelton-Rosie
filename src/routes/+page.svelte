46ba767fd663138f4e8e802a9a0221addee2d347
Oscar Welton-Rosie <oweltonrosie@proton.me>
2026-09-21 10:33:46 +1200

    missed a bit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 
 <p>I'm Oscar. I'm a New Zealand Year 12 high school student and hobby developer working with <a href="https://github.com/OWelton-Rosie?tab=repositories&q=&type=&language=svelte&sort=" target="_blank" rel="noopener noreferrer" aria-label="GitHub">Svelte</a> and <a href="https://github.com/OWelton-Rosie?tab=repositories&q=&type=&language=python&sort=" target="_blank" rel="noopener noreferrer" aria-label="GitHub">Python</a>.</p>
 
-<p>When not coding, I enjoy <a href="https://www.strava.com/athletes/120805648" target="_blank" rel="noopener noreferrer" aria-label="Strava">running, cycling</a>, <a href="https://www.worldcubeassociation.org/persons/2023WELT02" target="_blank" rel="noopener noreferrer">solving Rubik's Cubes</a>, and <a href="/writing">writing</a>.</p>
+<p>When not coding, I enjoy <a href="https://www.strava.com/athletes/120805648" target="_blank" rel="noopener noreferrer" aria-label="Strava">running, cycling</a>, <a href="https://www.worldcubeassociation.org/persons/2023WELT02" target="_blank" rel="noopener noreferrer" aria-label="WCA">solving Rubik's Cubes</a>, and <a href="/writing">writing</a>.</p>
 
 <Footer />
 
